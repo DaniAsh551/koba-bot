6bc1f6f67b1afff22873ef503cef5f0c867560bb
d4n1 <d4n1.551@gmail.com>
2020-11-05 14:38:26 +0500

    fix: games

--- src/handlers/games.js
+++ src/handlers/games.js
@@ -6,11 +6,12 @@ const KEY = "games";
  * @param {{ args:string[], message:Message, config:any }} param0
  */
 async function games({ args, message, config }) {
-  let games = config.play.games
+  let games = config.play.games;
+  let game_help = games
     .map((g) => `${g.name}:  Tags '${g.tags.join(", ")}' supported.`)
     .join("\n");
   return `Hi <@${message.member.user.id}> - The list of games we officialy support as of now:
-  ${games}
+  ${game_help}
   example: '${config.prefix}play ${games[0].tags[0]}' invites people to a game of ${games[0].name}`;
 }
 
