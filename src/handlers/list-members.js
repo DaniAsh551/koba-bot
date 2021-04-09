8d973903d8d8cc099be5685ea574ac774cb4cb7a
d4n1 <d4n1.551@gmail.com>
2021-04-09 15:47:21 +0500

    wip

--- src/handlers/list-members.js
+++ src/handlers/list-members.js
@@ -1,5 +1,5 @@
 const { Message, Client, MessageEmbed } = require("discord.js");
-const KEY = "group";
+const KEY = "listmembers";
 const { createHandler, EVENT_TYPE } = require("../createHandler");
 const { getUserProp } = require("../helper");
 
@@ -8,18 +8,35 @@ const { getUserProp } = require("../helper");
  * @param {{ args:string[], message:Message, config:any, client:Client }} param0
  */
 async function help({ args, message, config, handlers }) {
-  let { prefix } = config;
+  let roles =
+    Array.from(message.mentions.roles.keys()).map((k) => ({
+      key: k,
+      role: message.mentions.roles.get(k),
+    })) || [];
+
+  if (roles.length < 1) {
+    return "No roles mentioned.";
+  }
+
+  let members = roles
+    .flatMap(({ role }) =>
+      Array.from(role.members.keys()).map((k) => role.members.get(k).user)
+    )
+    .map((u) => getUserProp(u, "name"));
+
+  console.log(members);
+  return "lol";
 
   let variables = [...args];
   let noOfGroups = parseInt(variables[0]);
 
   if (isNaN(noOfGroups))
-    return "Need a no of groups to be specified. Usage: [noOfGroups] [Comma Seperated List]";
+    return "Need a no of groups to be specified. Usage: [noOfGroups] [Space Seperated List]";
 
   if (noOfGroups < 2)
     return "Need atleast 2 groups to divide into, right? I mean, there's no meaning to trying to group otherwise.";
 
-  variables = variables[1].split(",");
+  variables.splice(0, 1);
 
   if (noOfGroups >= variables.length)
     return "No need to divide into groups, each member is a group entirely already. In other words, too less members for too many groups.";
@@ -62,5 +79,5 @@ module.exports = createHandler(
   KEY,
   help,
   EVENT_TYPE.MESSAGE,
-  "Groups a given set of messages into specified no of groups. Usage: [noOfGroups] [Comma Seperated List]"
+  "Lists members in the specified roles."
 );
