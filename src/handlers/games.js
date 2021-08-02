25d48245aa9c8978ae9e0f60de65d47217200a78
Dani <>
2021-08-02 16:33:15 +0500

    fix: games command

--- src/handlers/games.js
+++ src/handlers/games.js
@@ -1,19 +1,38 @@
-const { Message, Client } = require("discord.js");
-const KEY = "games";
+const { Message, Client, MessageEmbed } = require("discord.js");
 const { createHandler, EVENT_TYPE } = require("../createHandler");
+const {getConfig} = require("../config");
+const {getUserProp} = require("../helper");
+const colors = require("../colors.json");
+const KEY = "games";
 
 /**
  * Spits out the list of supported games.
  * @param {{ args:string[], message:Message, config:any, client: Client }} param0
  */
 async function games({ args, message, config }) {
-  let games = config.play.games;
-  let game_help = games
-    .map((g) => `\n${g.name}:  Tags '${g.tags.join(", ")}' supported.`)
-    .join("\n");
-  return `Hi <@${message.member.user.id}> - The list of games we officialy support as of now:
-  \`\`\`${game_help}\`\`\`
-  example: \`\`\`${config.prefix}play ${games[0].tags[0]}\`\`\` invites people to a game of ${games[0].name}`;
+  let playConfig = getConfig(message.channel.guild.id, "play.json");
+  let gameNames = playConfig.games.map(g => g.name).sort();
+  let games = gameNames.map(name => playConfig.games.filter(g => g.name == name)[0]).filter(x => x);
+
+  let embed = new MessageEmbed()
+    .setColor(colors.blue_crayola.hex)
+    .setTitle(
+      `Hi ${getUserProp(
+        message.member.user,
+        "name"
+      )}! The list of games officialy supported on this Guild as of now:`
+    );
+
+  let fields = games.map((g) => ({
+    name: g.name,
+    value: `Supported tags:
+        ${g.tags.map(t => `    - ${t}`).join('\r\n')}
+        
+        `,
+  }));
+
+  embed.addFields(fields);
+  return embed;
 }
 
 module.exports = createHandler(
