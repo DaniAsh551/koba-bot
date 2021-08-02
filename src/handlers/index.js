3ccc3203138c53f117017c37a2ca0f888ad7a300
Dani <>
2021-08-02 15:52:10 +0500

    fix: null handlers handling

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -5,5 +5,6 @@ fs.readdirSync(__dirname)
   .filter((x) => x !== "index.js")
   .forEach(function (file) {
     let modExport = require(path.join(__dirname, file));
-    module.exports[modExport.KEY] = modExport;
+    if(modExport.KEY)
+      module.exports[modExport.KEY] = modExport;
   });
