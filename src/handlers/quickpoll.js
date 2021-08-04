82a73b639f54d8edd036d57083c05f634af1323b
Dani <>
2021-08-04 14:43:41 +0500

    feat: quickpoll

--- src/handlers/quickpoll.js
+++ src/handlers/quickpoll.js
@@ -1,15 +1,16 @@
-const { Message, Client, MessageEmbed } = require("discord.js");
+const { Message, Client, MessageEmbed, Guild, Emoji } = require("discord.js");
 const { createHandler, EVENT_TYPE } = require("../createHandler");
 const { getUserProp } = require("../helper");
 const KEY = "quickpoll";
 
 const MAX_OPTIONS = 26;
+const emojis = "🇦 🇧 🇨 🇩 🇪 🇫 🇬 🇭 🇮 🇯 🇰 🇱 🇲 🇳 🇴 🇵 🇶 🇷 🇸 🇹 🇺 🇻 🇼 🇽 🇾 🇿".split(' ');
 
 /**
  *
  * @param {{ args:string[], message:Message, config:any, client:Client }} param0
  */
-async function quickpoll({ args, message, config, handlers }) {
+async function quickpoll({ args, message, config, handlers, client }) {
   let { prefix } = config;
   let chunks = message.content.split('|');
   if(chunks.length > 26)
@@ -19,38 +20,43 @@ async function quickpoll({ args, message, config, handlers }) {
   chunks[0] = chunks[0].substr(remLength);
 
   let channel = message.channel;
+
   let reactions = chunks
-    .filter((_,i) => i>0).map((_,i) => getReaction(i));
+    .filter((_,i) => i>0).map((_,i) => getEmoji(i));
 
-  let embed = new MessageEmbed()
-    .setColor("#0099ff")
-    .setTitle(
-      `${getUserProp(
-        message.member.user,
-        "mention"
-      )} asks:
-      ${chunks[0]}
+  // let response = new MessageEmbed()
+  //   .setColor("#0099ff")
+  //   .setTitle(
+  //     `${getUserProp(
+  //       message.member.user,
+  //       "name"
+  //     )} asks:` + "\n" + chunks[0]
+  //   );
 
-      ${chunks.filter((_,i) => i>0).map((o,i) => `${reactions[i]} ${o}`)}
-      `
-    );
-    
-  channel.send(embed)
+  // response.addField(" ",
+  //   chunks.filter((_,i) => i>0).map((o,i) => `${reactions[i]} ${o}`).join('\n'));
+
+  let response = `${getUserProp(message.member.user,"mention")} asks:
+  *${chunks[0].trimEnd()}*\n` + 
+  `${chunks.filter((_,i) => i>0).map((o,i) => `   ${reactions[i]} ${o}`).join('\n')}
+  `;
+
+  channel.send(response)
   .then(message => {
       reactions.forEach(r => message.react(r))
   })
   .catch(reason => {});
+  message.delete();
 }
 
 //get reaction options for poll (A-Z)
-function getReaction(index){
+function getEmoji(index){
     if(index >= 26)
         return null;
 
-    const reactions = Array.from(Array(26)).map(i => String.fromCharCode(97 + i))
-    .map(c => `:regional_indicator_${c}:`);
-    return reactions[index];
+    return emojis[index];
 }
+
 
 module.exports = createHandler(
   KEY,
